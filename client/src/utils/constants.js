8fbecc2b815d1e0568518ca250cbd62f14a7c148
damangulati18-maker <damangulati18@gmail.com>
2026-07-15 12:46:24 +0530

    fix: use env variable for backend API URL

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,9 +1,11 @@
-export const signupurl="http://localhost:5004/signupNewUser";
-export const loginurl="http://localhost:5004/loginUser";
-export const getitemsurl="http://localhost:5004/getItems";
-export const additemurl="http://localhost:5004/addNewItem";
-export const assignitemurl="http://localhost:5004/assignItem";
-export const getassignitemsurl="http://localhost:5004/getAssignedItems";
-export const additemforrepairurl="http://localhost:5004/addItemForMaintainance";
-export const getrepairitemsurl="http://localhost:5004/getItemsForRepairs";
-export const logouturl="http://localhost:5004/logout";
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+export const signupurl = `${BASE_URL}/signupNewUser`;
+export const loginurl = `${BASE_URL}/loginUser`;
+export const getitemsurl = `${BASE_URL}/getItems`;
+export const additemurl = `${BASE_URL}/addNewItem`;
+export const assignitemurl = `${BASE_URL}/assignItem`;
+export const getassignitemsurl = `${BASE_URL}/getAssignedItems`;
+export const additemforrepairurl = `${BASE_URL}/addItemForMaintainance`;
+export const getrepairitemsurl = `${BASE_URL}/getItemsForRepairs`;
+export const logouturl = `${BASE_URL}/logout`;
